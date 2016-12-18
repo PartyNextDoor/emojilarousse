94415afc108675d5d86b3def605ac1cfa0f23bd7
PartyNextDoor <princeketeleer@mbp-de-prince-3.attlocal.net>
2016-12-18 09:27:08 +0100

    boolean fun

--- Prince/ViewController.swift
+++ Prince/ViewController.swift
@@ -9,8 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-
+    
+    
     @IBOutlet weak var thelabel: UILabel!
     
     @IBOutlet weak var text1: UITextField!
@@ -19,21 +19,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var text2: UITextField!
     
     @IBAction func Tap(sender: AnyObject) {
-    
-      thelabel.text = "Answer: \(Double(text1.text!)! + Double(text2.text!)!)"
         
+        let addition = true
+        
+        if addition {
+            thelabel.text = "Answer: \(Double(text1.text!)! + Double(text2.text!)!)"
+        } else {
+            thelabel.text = "Answer: \(Double(text1.text!)! - Double(text2.text!)!)"
         }
+    }
     
     override func viewDidLoad() { thelabel.text = "prince"
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
